e3bcb086423bf4908b391b5eef1c6de90a0a89f7
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2023-04-27 21:32:07 +0200

    Fixing CORS

--- infrastructure/auth.tf
+++ infrastructure/auth.tf
@@ -72,75 +72,10 @@ resource "aws_lambda_function_url" "auth" {
 
   cors {
     allow_credentials = true
-    allow_origins     = ["*"]
+    allow_origins     = ["OPTIONS,POST"]
     allow_methods     = ["*"]
     allow_headers     = ["date", "keep-alive"]
     expose_headers    = ["keep-alive", "date"]
     max_age           = 86400
   }
-}
-
-
-resource "aws_api_gateway_rest_api" "auth" {
-  name        = "AuthFunctionAPI"
-  description = "API for auth function"
-}
-
-resource "aws_api_gateway_resource" "auth" {
-  rest_api_id = "${aws_api_gateway_rest_api.auth.id}"
-  parent_id   = "${aws_api_gateway_rest_api.auth.root_resource_id}"
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "auth" {
-  rest_api_id   = "${aws_api_gateway_rest_api.auth.id}"
-  resource_id   = "${aws_api_gateway_resource.auth.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "auth" {
-  rest_api_id = "${aws_api_gateway_rest_api.auth.id}"
-  resource_id = "${aws_api_gateway_method.auth.resource_id}"
-  http_method = "${aws_api_gateway_method.auth.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.auth.invoke_arn}"
-}
-
-resource "aws_api_gateway_method" "auth_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.auth.id}"
-  resource_id   = "${aws_api_gateway_rest_api.auth.root_resource_id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "auth_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.auth.id}"
-  resource_id = "${aws_api_gateway_method.auth_root.resource_id}"
-  http_method = "${aws_api_gateway_method.auth_root.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.auth.invoke_arn}"
-}
-
-resource "aws_api_gateway_deployment" "example" {
-  depends_on = [
-    aws_api_gateway_integration.auth,
-    aws_api_gateway_integration.auth_root,
-  ]
-
-  rest_api_id = "${aws_api_gateway_rest_api.auth.id}"
-  stage_name  = "prod"
-}
-
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.auth.function_name}"
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.auth.execution_arn}/*/*"
 }
