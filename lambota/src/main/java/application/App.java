9889140bc934660b6367830559e243d2ee30a15f
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2023-07-13 23:18:43 +0200

    Removing function

--- lambota/src/main/java/application/App.java
+++ lambota/src/main/java/application/App.java
@@ -2,6 +2,11 @@ package application;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -10,12 +15,22 @@ import java.time.format.DateTimeFormatter;
 import java.util.Base64;
 import java.util.Properties;
 import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.*;
 
+import com.google.cloud.texttospeech.v1.SynthesizeSpeechResponse;
+import com.google.cloud.texttospeech.v1.TextToSpeechClient;
+import com.google.cloud.texttospeech.v1.AudioConfig;
+import com.google.cloud.texttospeech.v1.AudioEncoding;
+import com.google.cloud.texttospeech.v1.SynthesisInput;
+import com.google.cloud.texttospeech.v1.VoiceSelectionParams;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.cloud.speech.v1p1beta1.*;
 
 public class App {
 
@@ -44,9 +59,12 @@ public class App {
             String audioFile = String.format("audio_%s_%s.mp3", timestamp, shortHash);
             recordAudio(audioFile);
 
-            uploadToBucket(storage, audioFile);
-            playAudioFromBucketWithBackoff(storage, audioFile);
-            cleanPromptFileFromBucket(storage, audioFile);
+            String uri = uploadToBucket(storage, PROMPTS_BUCKET, audioFile);
+            String transcription = transcribeAudio(uri);
+            String response = askOpenAI(gcp.getProperty("openai_key"), transcription);
+            String responsePath = synthesizeSpeechAndSaveToFile(response, String.format("res_%s", audioFile));
+            playAudio(responsePath);
+            uploadToBucket(storage, RESPONSES_BUCKET, audioFile);
 
             System.out.println("Continue recording... (cut with CTRL+C)");
         }
@@ -65,79 +83,33 @@ public class App {
         }
     }
 
-    private static void uploadToBucket(Storage storage, String filePath) {
+    private static String uploadToBucket(Storage storage, String bucket, String filePath) {
         try {
             File file = new File(filePath);
             String key = file.getName();
 
-            Blob blob = storage.get(PROMPTS_BUCKET, key);
+            Blob blob = storage.get(bucket, key);
             if (blob != null) {
                 System.out.println("File already exists in the bucket. Skipping upload.");
                 file.delete();
-                return;
+                return "";
             }
 
-            storage.create(Blob.newBuilder(PROMPTS_BUCKET, key).build(), Files.readAllBytes(file.toPath()));
+            storage.create(Blob.newBuilder(bucket, key).build(), Files.readAllBytes(file.toPath()));
             file.delete();
+            return String.format("gs://%s/%s", bucket, key);
         } catch (Exception e) {
             System.out.println("Error uploading file to bucket: " + e.getMessage());
         }
+        return "";
     }
 
-    private static void playAudioFromBucketWithBackoff(Storage storage, String key) {
-        int maxRetries = 5;
-        int initialDelay = 10;
-
-        for (int attempt = 0; attempt <= maxRetries; attempt++) {
-            try {
-                Blob blob = storage.get(RESPONSES_BUCKET, key);
-                if (blob != null) {
-                    System.out.println("Playing audio response...");
-
-                    File tempFile = File.createTempFile("temp", ".mp3");
-                    blob.downloadTo(tempFile.toPath());
-
-                    playAudio(tempFile.getAbsolutePath());
-
-                    tempFile.delete();
-                    blob.delete();
-
-                    System.out.println("Finished playing audio response!");
-                    break;
-                } else {
-                    System.out.println("No audio file found in the bucket");
-                    if (attempt < maxRetries) {
-                        int sleepTime = initialDelay * (int) Math.pow(2, attempt);
-                        System.out.println("Retrying in " + sleepTime + " seconds...");
-                        Thread.sleep(sleepTime * 1000);
-                    } else {
-                        System.out.println("Reached the maximum number of retries. Giving up.");
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
-                if (attempt < maxRetries) {
-                    int sleepTime = initialDelay * (int) Math.pow(2, attempt);
-                    System.out.println("Retrying in " + sleepTime + " seconds...");
-                    try {
-                        Thread.sleep(sleepTime * 1000);
-                    } catch (InterruptedException ex) {
-                        System.out.println("InterruptedException occurred: " + ex.getMessage());
-                    }
-                } else {
-                    System.out.println("Reached the maximum number of retries. Giving up.");
-                    break;
-                }
-            }
-        }
-    }
-
-    private static void cleanPromptFileFromBucket(Storage storage, String key) {
+    private static void cleanPromptFileFromBucket(Storage storage, String bucket, String key) {
         try {
-            storage.delete(PROMPTS_BUCKET, String.format("prompts/%s", key));
-            System.out.printf("Object '%s' successfully deleted from bucket '%s'%n", key, PROMPTS_BUCKET);
+            storage.delete(bucket, String.format("prompts/%s", key));
+            System.out.printf("Object '%s' successfully deleted from bucket '%s'%n", key, bucket);
         } catch (Exception e) {
-            System.out.printf("Error deleting object '%s' from bucket '%s': %s%n", key, PROMPTS_BUCKET, e.getMessage());
+            System.out.printf("Error deleting object '%s' from bucket '%s': %s%n", key, bucket, e.getMessage());
         }
     }
 
@@ -163,5 +135,94 @@ public class App {
         } catch (IOException | InterruptedException e) {
             System.out.println("Error occurred while playing audio: " + e.getMessage());
         }
+    }
+
+    private static String transcribeAudio(String gcsUri) {
+        try {
+            SpeechClient client = SpeechClient.create();
+
+
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setUri(gcsUri).build();
+
+            RecognitionConfig config = RecognitionConfig.newBuilder()
+                    .setEncoding(RecognitionConfig.AudioEncoding.MP3)
+                    .setLanguageCode("nl-NL")
+                    .build();
+
+            LongRunningRecognizeResponse response = client.longRunningRecognizeAsync(config, audio).get();
+
+            StringBuilder transcript = new StringBuilder();
+            for (SpeechRecognitionResult result : response.getResultsList()) {
+                for (SpeechRecognitionAlternative alternative : result.getAlternativesList()) {
+                    transcript.append(alternative.getTranscript()).append(" ");
+                }
+            }
+            return transcript.toString().trim();
+        } catch (Exception e) {
+            System.out.println("Error transcribing audio: " + e.getMessage());
+            return "";
+        }
+    }
+
+    private static String askOpenAI(String apiKey, String inputPrompt) {
+        try {
+            JsonObject data = new JsonObject();
+            data.addProperty("model", "text-davinci-003");
+            data.addProperty("prompt", "You're a Dutch teacher and you are practicing speaking with a student. Your answer should only be what the Dutch teacher answers. The student says: " + inputPrompt);
+            data.addProperty("max_tokens", 100);
+
+            Gson gson = new Gson();
+            String requestData = gson.toJson(data);
+
+            String apiEndpoint = "https://api.openai.com/v1/completions";
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(apiEndpoint))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + apiKey)
+                    .POST(HttpRequest.BodyPublishers.ofString(requestData, StandardCharsets.UTF_8))
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            String responseBody = response.body();
+            System.out.println(responseBody);
+
+            JsonObject responseJson = JsonParser.parseString(responseBody).getAsJsonObject();
+            return responseJson.getAsJsonArray("choices").get(0).getAsJsonObject().get("text").getAsString();
+        } catch (Exception e) {
+            System.out.println("Error occurred while querying OpenAI: " + e.getMessage());
+            return "";
+        }
+    }
+
+    public static String synthesizeSpeechAndSaveToFile(String text, String filePath) {
+        try {
+            TextToSpeechClient client = TextToSpeechClient.create();
+
+            SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
+
+            VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+                    .setLanguageCode("nl-NL")
+                    .setName("nl-NL-Wavenet-B")
+                    .build();
+
+            AudioConfig audioConfig = AudioConfig.newBuilder()
+                    .setAudioEncoding(AudioEncoding.MP3)
+                    .build();
+
+            SynthesizeSpeechResponse response = client.synthesizeSpeech(input, voice, audioConfig);
+
+            byte[] audioContent = response.getAudioContent().toByteArray();
+
+            Path path = Path.of(filePath);
+            Files.write(path, audioContent, StandardOpenOption.CREATE);
+
+            System.out.println("Audio file saved to: " + filePath.toString());
+            return path.toString();
+        } catch (Exception e) {
+            System.out.println("Error synthesizing speech and uploading to bucket: " + e.getMessage());
+        }
+        return null;
     }
 }
